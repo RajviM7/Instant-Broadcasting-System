7a7112653952b482b0dc82a6f5d8ac6613336357
RajviM7 <rajvi.m7@gmail.com>
2017-05-19 12:00:59 -0400

    Final

---
+++ SUB/testcases.ts
@@ -0,0 +1,7 @@
+//Here are the test cases
+
+Test: 															Result:
+Front Office to Risk Office: Meeting next weeek    				Passed
+Risk Office Operations:	Finish client project					Passed
+Operations to Finance: Verify profit earnings					Passed
+Finance to Operations: Completed								Passed
--- chat_app.js
+++ chat_app.js
@@ -1,23 +1,36 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var nickname = "";
-var roomno = 1;
+var connectedUsers = {};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  io.emit('chat message', 'A user has connected');
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-  socket.on('disconnect', function(){
-    io.emit('chat message', 'A user has disconnected');
-  });
+
+	socket.on('user connects', function(user, team){
+		io.emit('user connects', '<strong>' + user + 'from team ' + team + ' has connected </strong>');
+		connectedUsers[user] = socket;
+	});
+
+	socket.on('user disconnects', function(user){
+		io.emit('user disconnects', user);
+	});
+
+	socket.on('chat message', function(msg, user){
+		io.emit('chat message', msg, user);
+	});
+
+	socket.on('user is typing', function(user){
+		io.emit('user is typing', user);
+	});
+
+	socket.on('user stopped typing', function(user){
+		io.emit('user stopped typing', user);
+	});
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
+http.listen(3000, function(){
+	console.log('listening on *:3000');
 });
