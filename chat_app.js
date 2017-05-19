4cfade8243fd90429f3c691f33f060161f81d6d3
Christopher Kim <christopherkim511@gmail.com>
2017-05-19 10:10:58 -0400

    Add files via upload
    Added the app and index

--- chat_app.js
+++ chat_app.js
@@ -1,23 +1,42 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var nickname = "";
-var roomno = 1;
+var teamAUsers = {};
+var teamBUsers = {};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  io.emit('chat message', 'A user has connected');
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-  socket.on('disconnect', function(){
-    io.emit('chat message', 'A user has disconnected');
-  });
+
+	socket.on('user connects', function(user, team){
+		io.emit('user connects', '<strong>' + user + ' from Team ' + team + ' has connected </strong>');
+		if (team == "A") {
+			teamAUsers[user] = socket.id;
+			socket.join('team A');
+		} else {
+			teamBUsers[user] = socket.id;
+			socket.join('team B');
+		}
+	});
+	socket.on('user disconnects', function(user){
+		io.emit('user disconnects', user);
+	});
+
+	socket.on('chat message', function(msg, user, team, checker){
+		io.emit('chat message', msg, user, team, checker);
+	});
+
+	socket.on('user is typing', function(user){
+		io.emit('user is typing', user);
+	});
+
+	socket.on('user stopped typing', function(user){
+		io.emit('user stopped typing', user);
+	});
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
+http.listen(3000, function(){
+	console.log('listening on *:3000');
 });
